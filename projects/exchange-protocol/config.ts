f105db8043a35ed3f8799e9dce3bff937394993d
acentior <acentior@outlook.com>
2022-06-15 07:50:11 +0400

    build: PancakeZapV1 was deployed

--- projects/exchange-protocol/config.ts
+++ projects/exchange-protocol/config.ts
@@ -1,14 +1,14 @@
 export default {
   PancakeRouter: {
-    mainnet: "0x10ed43c718714eb63d5aa57b78b54704e256024e",
-    testnet: "0xD99D1c33F9fC3444f8101754aBC46c52416550D1",
+    mainnet: "0x0000000000000000000000000000000000000000",
+    testnet: "0x07810d21eAf7c5A8F92Eae3eBfa83e4bE450Fc6F",
   },
   MaxZapReverseRatio: {
     mainnet: "50", // 0.5%
     testnet: "50", // 0.5%
   },
   WBNB: {
-    mainnet: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-    testnet: "0x094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F",
+    mainnet: "0x0000000000000000000000000000000000000000",
+    testnet: "0xb75E979162827b8e71e8B144C5962d11aA5ce738",
   },
 };
